0a89c807264932d87ded41d2b911954c7195a732
Jean Duong <jeanduong@Jeans-iMac.local>
2022-02-07 15:05:47 -0700

    fixed problem1

---
+++ coding-challenges/src/main/java/com/revature/Main.java
@@ -0,0 +1,9 @@
+package com.revature;
+
+public class Main {
+    /* Use this class to populate your Problem(s) class */
+
+    public static void main(String[] args) {
+        System.out.println("Start in Main!");
+    }
+}
--- coding-challenges/src/main/java/com/revature/Problem1.java
+++ coding-challenges/src/main/java/com/revature/Problem1.java
@@ -2,12 +2,6 @@ package com.revature;
 
 public class Problem1 {
     public int sumOfArray(int[] arr) {
-        int sum = 0;
-
-        for (int n : arr) {
-            sum += n;
-        }
-
-        return sum;
+        return 0;
     }
 }
