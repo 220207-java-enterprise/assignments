dddbe9868dc17efa7e9af58334212d1430420d8d
Ryan Odeneal <ryan063@revature.net>
2022-02-08 15:51:15 -0600

    sum-of-array exercise

---
+++ coding-challenges/sum-of-array/SumOfArray.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfArray{
+	public static void main(String[] args){
+		int integerArray[] = {1, 2, 3, 4, 5};
+		
+		int sum = 0;
+		int i;
+		for (i = 0; i < integerArray.length; i++) {
+			sum = sum + integerArray[i];
+		}
+		System.out.println("The sum of 1, 2, 3, 4, and 5 is " + sum);
+	}
+}
