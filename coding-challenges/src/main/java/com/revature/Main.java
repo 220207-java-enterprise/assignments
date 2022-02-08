0d8e1536bab959f533a76fc3495e480aea6dcc7a
Jean Duong <jeanduong@Jeans-iMac.local>
2022-02-08 09:43:50 -0700

    create problem dir

--- coding-challenges/src/main/java/com/revature/Main.java
+++
@@ -1,9 +0,0 @@
-package com.revature;
-
-public class Main {
-    /* Use this class to populate your Problem(s) class */
-
-    public static void main(String[] args) {
-        System.out.println("Start in Main!");
-    }
-}
--- coding-challenges/src/main/java/com/revature/Problem1.java
+++
@@ -1,7 +0,0 @@
-package com.revature;
-
-public class Problem1 {
-    public int sumOfArray(int[] arr) {
-        return 0;
-    }
-}
