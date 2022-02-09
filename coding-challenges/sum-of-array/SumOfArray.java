08ca24cb3827bbcd04782eab4b56b245ba746404
Cameron Lintz <cameron034@revature.net>
2022-02-09 09:36:35 -0500

    I hope this is right

---
+++ coding-challenges/sum-of-array/SumOfArray.java
@@ -0,0 +1,15 @@
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int a[] = {1, 2, 3, 4, 5};
+		int sum=0;
+	
+		for (int i=0; i<5; i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.print("Sum of Array elements:"+sum);
+
+
+}
+}
