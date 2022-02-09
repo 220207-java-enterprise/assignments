e789bb6559aab14e423ab6b43273343a58a2727a
ebewebdev <ebewebdev@gmail.com>
2022-02-09 09:38:55 -0500

    sumofarray solution

---
+++ coding-challenges/sum-of-array/SumOfArray.java
@@ -0,0 +1,16 @@
+public class SumOfArray {
+    
+    public static void main (String[] args)
+    {
+        
+    	int myArray = {1,2,3,4,5};
+    	int sum = 0;
+    	for (int i = 0; i < myArray.length; i++){
+        
+	    	sum += myArray[i];
+			
+    	}
+          System.out.println(sum);
+
+    }
+}
