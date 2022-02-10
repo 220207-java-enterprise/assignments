ac82da02c0d2e847366834eef6ab41374b0fed68
Jean Duong <jeanduong@Jeans-iMac.local>
2022-02-10 09:47:57 -0700

    fix merge conflicts

--- coding-challenges-solutions/length-of-string/LengthOfString.java
+++ coding-challenges-solutions/length-of-string/LengthOfString.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class LengthOfString {
 	public static void main(String[] args) {
 		String input = "";
-		String abbr = "";
+		
 		Scanner scan = new Scanner(System.in);
 		
-		input = scan.nextLine();
+		input = scan.next();
 		
 		if (input.length() > 10) {
-			System.out.println(input.charAt(0) + String.valueOf(input.length() - 2) + input.charAt(input.length() - 1));	
+			System.out.println(input.charAt(0) + String.valueOf(input.length() - 2) + input.charAt(input.length() -1));
 		} else {
 			System.out.println(input);
 		}
--- coding-challenges-solutions/sum-of-array/SumOfArray.java
+++ coding-challenges-solutions/sum-of-array/SumOfArray.java
@@ -1,10 +1,11 @@
 public class SumOfArray {
 	public static void main(String[] args) {
 		int sum = 0;
-		int[] arr = { 1, 2, 3, 4, 5};
+		int[] arr = {1, 2, 3, 4 ,5};
+		char[] cArr = {'a', 'b', 'c'};
 		
-		for (int n : arr) {
-			sum += n;
+		for (int num : arr) {
+			sum = sum + num;	
 		}
 		
 		System.out.println(sum);
