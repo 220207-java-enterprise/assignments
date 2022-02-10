84474051c2079ca3523a256971455b76a13aa7db
zhenying <a664330348@gmail.com>
2022-02-10 17:07:29 -0500

    completed hangman

--- coding-projects-demos/src/com/revature/hangman/Game.java
+++ coding-projects-demos/src/com/revature/hangman/Game.java
@@ -1,5 +1,6 @@
 package com.revature.hangman;
 
+import java.util.Stack;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,20 +10,53 @@ public class Game {
         char input = ' ';
         String word = getWord();
         String hidden = hideWord(word);
+        Stack<Character> answer = setUpAnswer(word);
         Scanner scan = new Scanner(System.in);
+        Stack<Character> finds = new Stack<>();
 
+        System.out.println("Welcome to Hangman!");
         while (life >= 0) {
-            System.out.println("Welcome to Hangman!");
+            if (answer.size() == finds.size()){
+                System.out.println("You won, you figure out the correct word: "+ word);
+                break;
+            }
             System.out.println("Word to guess: " + hidden);
             System.out.println("Life: " + life);
 
             System.out.print("Guess: ");
             input = scan.nextLine().charAt(0);
 
-            if (validation(word, input)) {
+            if (validation(input, finds, answer)) {
+                hidden = upDateHidden(word,finds);
                 System.out.println("Yay!");
+            }else{
+                life--;
             }
         }
+    }
+
+    private String upDateHidden(String word, Stack<Character> finds) {
+        String hidden = "";
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+            if (finds.search(c) == -1){
+                hidden += '*';
+            }else{
+                hidden += c;
+            }
+        }
+        return hidden;
+    }
+
+    private  Stack<Character> setUpAnswer(String word) {
+        Stack<Character> ans = new Stack<>();
+        for(int i=0; i<word.length(); i++){
+            Character c = word.charAt(i);
+            if (ans.search(c) == -1){
+                ans.push(c);
+            }
+        }
+        return ans;
     }
 
     private String getWord() {
@@ -45,13 +79,19 @@ public class Game {
         return hidden;
     }
 
-    private Boolean validation(String word, char input) {
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == input) {
-                return true;
+    private Boolean validation(char input, Stack<Character> finds, Stack<Character> ans) {
+        //check if the character is already finds
+        for(char c:finds){
+            if(input == c){
+                System.out.println("You already guess this one, try something else");
+                return false;
             }
         }
-
+        Character c = input;
+        if (ans.search(c) != -1 && finds.search(c) == -1){
+            finds.push(c);
+            return  true;
+        }
         return false;
     }
 }
