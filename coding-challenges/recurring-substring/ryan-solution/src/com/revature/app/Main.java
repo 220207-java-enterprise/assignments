bda67a6d97d0569a4ab01bfb393f4e0686f4761e
Ryan Odeneal <ryan063@revature.net>
2022-02-10 10:20:57 -0600

    More work on recurring-substring. Still need to work on portion where I append the substrings until I get something as long or longer than the original string and compare.

--- coding-challenges/recurring-substring/ryan-solution/src/com/revature/app/Main.java
+++ coding-challenges/recurring-substring/ryan-solution/src/com/revature/app/Main.java
@@ -2,6 +2,7 @@ package com.revature.app;
 
 public class Main {
     public static void main(String[] args) {
-        /* Use Main to enter in values into your Solution class */
+        Solution test = new Solution();
+        test.recurringSubstring("abcdefg");
     }
 }
--- coding-challenges/recurring-substring/ryan-solution/src/com/revature/app/Solution.java
+++ coding-challenges/recurring-substring/ryan-solution/src/com/revature/app/Solution.java
@@ -2,7 +2,15 @@ package com.revature.app;
 
 public class Solution {
     public boolean recurringSubstring(String s) {
-        /* Write your code here */
+        int i, j = 0;
+        for(i = 0; i < s.length(); i++) {
+            for(j = 0; j < s.substring(i).length(); j++){
+                //substring starting at i
+                String iSubString = s.substring(i);
+                String jSubstring = iSubString.substring(0, iSubString.length() - j);
+
+            }
+        }
         return false;
     }
 }
