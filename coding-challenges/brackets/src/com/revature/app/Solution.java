fefa47c3711dc501635d320e786930c6e37fca0a
abhilekhx <lekh7adhikari@gmail.com>
2022-02-10 10:58:23 -0500

    brackets coding-challenge DONE

--- coding-challenges/brackets/src/com/revature/app/Solution.java
+++ coding-challenges/brackets/src/com/revature/app/Solution.java
@@ -3,6 +3,15 @@ package com.revature.app;
 public class Solution {
     public String brackets(String s) {
         /* Write your code here */
-        return "";
+        String result = new String("");
+
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i)=='.'){
+                result += "[.]";
+            } else {
+                result += s.charAt(i);
+            }
+        }
+        return result;
     }
 }
