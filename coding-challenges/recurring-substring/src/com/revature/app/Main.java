67a7f643591f52cff0e6c91a024e22a033793450
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-02-10 10:55:15 -0800

    Rec Sub Solution

--- coding-challenges/recurring-substring/src/com/revature/app/Main.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Main.java
@@ -3,5 +3,9 @@ package com.revature.app;
 public class Main {
     public static void main(String[] args) {
         /* Use Main to enter in values into your Solution class */
+        String s = "abcabcabc";
+        Solution sl = new Solution();
+        System.out.println(sl.recurringSubstring(s));
+
     }
 }
--- coding-challenges/recurring-substring/src/com/revature/app/Solution.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Solution.java
@@ -3,6 +3,28 @@ package com.revature.app;
 public class Solution {
     public boolean recurringSubstring(String s) {
         /* Write your code here */
+        String str ;
+        for (int j=1; j <= s.length(); j++){
+            str = s.substring(0,j);
+            for (int i=0; i < (s.length()/str.length()); i++){
+                if ((s.length()/str.length())%2 == 0){
+                    str += str;
+                    if (s.equals(str)){
+                        return true;
+                    }
+
+                }
+                else if ((s.length()/str.length())%2 != 0){
+                    str += str;
+                    if (str.equals(s+s)){
+                        return true;
+                    }
+
+                }
+                
+                
+            }
+        }
         return false;
     }
 }
