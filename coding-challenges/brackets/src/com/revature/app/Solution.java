4354cf76e4bb4d925cd48dfcbffbbbbd3138e155
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-02-10 09:03:01 -0800

    Bracket Solutin

--- coding-challenges/brackets/src/com/revature/app/Solution.java
+++ coding-challenges/brackets/src/com/revature/app/Solution.java
@@ -3,6 +3,20 @@ package com.revature.app;
 public class Solution {
     public String brackets(String s) {
         /* Write your code here */
-        return "";
+        for (int i=0; i < s.length(); i++){
+            if (s.charAt(i) == '.'){
+                s = new StringBuilder(s).insert(i+1, "]").toString();
+
+            }
+
+        }
+        for (int i=s.length()-1; i >=0; i--){
+            if (s.charAt(i) == '.'){
+                s = new StringBuilder(s).insert(i, "[").toString();
+
+            }
+
+        }
+        return s;
     }
 }
