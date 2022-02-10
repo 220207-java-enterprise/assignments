7780c6d69bd98f864bc07bd7a478e55460d9c030
usertest0447 <paulsmith414@yahoo.com>
2022-02-09 22:42:14 -0600

    new proper branch add

--- coding-challenges/sum-of-array/SumOfArray.java
+++ coding-challenges/sum-of-array/SumOfArray.java
@@ -1,12 +1,13 @@
-public class SumOfArray {
-	public static void main(String[] args) {
-		int sum = 0;
-		int[] arr = { 1, 2, 3, 4, 5};
-		
-		for (int n : arr) {
-			sum += n;
-		}
-		
-		System.out.println(sum);
+class SumOfArray{
+    
+    public static void main(String[] args){
+        
+	int [] nums = {1, 2, 3, 4, 5};
+	int sum = 0;
+	
+	for (int num : nums){
+	    sum += num;
 	}
+	System.out.println("Sum: " + sum);
+    }
 }
--- java-koans/koans/app/data/src/beginner/AboutKoans.java
+++ java-koans/koans/app/data/src/beginner/AboutKoans.java
@@ -8,12 +8,13 @@ public class AboutKoans {
 
     @Koan
     public void findAboutKoansFile() {
-            }
+        fail("delete this line to advance");
+    }
 
     @Koan
     public void definitionOfKoanCompletion() {
         boolean koanIsComplete = false;
-        if (koanIsComplete) {
+        if (!koanIsComplete) {
             fail("what if koanIsComplete variable was true?");
         }
     }
