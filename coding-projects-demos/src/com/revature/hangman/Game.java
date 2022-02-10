2f2c1e667109158c87e2e2db93780013e9a65ac0
Aidan Amato <aidanamato@comcast.net>
2022-02-10 16:35:40 -0500

    working on updateWord function in hangman game

--- coding-projects-demos/src/com/revature/hangman/Game.java
+++ coding-projects-demos/src/com/revature/hangman/Game.java
@@ -1,18 +1,24 @@
 package com.revature.hangman;
 
-import java.util.Random;
-import java.util.Scanner;
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.*;
 
 public class Game {
     public void start() {
+        System.out.println("Welcome to Hangman!");
+
         int life = 6;
         char input = ' ';
         String word = getWord();
         String hidden = hideWord(word);
+
+        char[] letterBank = new char[26];
+        int letterCounter = 0;
+
         Scanner scan = new Scanner(System.in);
 
         while (life >= 0) {
-            System.out.println("Welcome to Hangman!");
             System.out.println("Word to guess: " + hidden);
             System.out.println("Life: " + life);
 
@@ -20,7 +26,10 @@ public class Game {
             input = scan.nextLine().charAt(0);
 
             if (validation(word, input)) {
-                System.out.println("Yay!");
+                String updateWord = updateWord(input, hidden, word);
+                hidden = updateWord;
+            } else {
+                life--;
             }
         }
     }
@@ -48,10 +57,48 @@ public class Game {
     private Boolean validation(String word, char input) {
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) == input) {
+
+
                 return true;
             }
         }
 
         return false;
     }
+
+    private String updateWord(char c, String hidden, String word) {
+        char[] wordArray = word.toCharArray();
+        char[] hiddenArray = hidden.toCharArray();
+
+        for (int i = 0; i < wordArray.length; i++) {
+            if (c == wordArray[i]) {
+                hiddenArray[i] = c;
+            }
+        }
+
+        hidden = hiddenArray.toString();
+        System.out.println(hiddenArray);
+
+        return hidden;
+    }
+
+    /*
+    private char[] updateLetterBank(char c, char[] charArr, int letterCounter) {
+        charArr
+
+
+        String updatedWord;
+
+        return updatedWord;
+    }
+
+    private int updateCounter(char c, char[] charArr, int letterCounter) {
+        charArr
+
+
+        String updatedWord;
+
+        return updatedWord;
+    }
+    */
 }
