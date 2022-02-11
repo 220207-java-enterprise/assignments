73e62418b5def4af5ca7967f1829e4351a9e3fcb
Aidan Amato <aidanamato@comcast.net>
2022-02-11 10:41:11 -0500

    retrieve user input for brackets coding challenge

--- coding-challenges/brackets/src/com/revature/app/Main.java
+++ coding-challenges/brackets/src/com/revature/app/Main.java
@@ -1,7 +1,17 @@
 package com.revature.app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /* Use this to enter in values for your Solution class */
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your string \n" +
+                         "> ");
+
+        String userInput = scan.nextLine();
+
+        new Solution().brackets(userInput);
     }
 }
