51a0de51794a997d243dd40d00aaa19343518f6e
Claire Abel <claire.m.abel@gmail.com>
2022-02-11 08:03:17 -0600

    commiting any changes

--- coding-challenges/brackets/src/com/revature/app/Main.java
+++ coding-challenges/brackets/src/com/revature/app/Main.java
@@ -3,5 +3,6 @@ package com.revature.app;
 public class Main {
     public static void main(String[] args) {
         /* Use this to enter in values for your Solution class */
+        String s = "Let's go.";
     }
 }
--- coding-challenges/brackets/src/com/revature/app/Solution.java
+++ coding-challenges/brackets/src/com/revature/app/Solution.java
@@ -1,8 +1,24 @@
 package com.revature.app;
 
+import java.util.*;
+
 public class Solution {
-    public String brackets(String s) {
-        String a ="abc.abc";
-        return "";
+
+    public void brackets(String s) {
+        System.out.print("Enter a string: ");
+        Scanner scr = new Scanner(System.in);
+    }
+
+    boolean check(String s) {
+        if (s == null) {
+            return false;
+        }
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if ((Character.isLetter(s.charAt(i)) == false)) {
+                return false;
+            }
+        }
     }
 }
+
