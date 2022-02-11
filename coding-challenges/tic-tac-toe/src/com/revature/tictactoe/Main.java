f0ec5248982711a6fe3325a7271df0b6f8197c57
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-02-11 14:06:36 -0800

    tictactoe

--- coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Main.java
+++ coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Main.java
@@ -2,5 +2,8 @@ package com.revature.tictactoe;
 
 public class Main {
     public static void main(String[] args) {
+        char[][] game = {{'O','O','O'},{'X','O','X'},{'O','X','X'}};
+        Solution s1 = new Solution();
+        System.out.println(s1.validateWin(game));
     }
 }
--- coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Solution.java
+++ coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Solution.java
@@ -2,6 +2,119 @@ package com.revature.tictactoe;
 
 public class Solution {
     public char validateWin(char[][] board) {
-        return ' ';
+        if(board[0][0] == 'X'){
+            if (board[0][1] == 'X'){
+                if (board[0][2] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+            if (board[1][0] == 'X'){
+                if (board[2][0] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+            if (board[1][1] == 'X'){
+                if (board[2][2] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+        }
+
+        else if(board[0][0] == 'O'){
+            if (board[0][1] == 'O'){
+                if (board[0][2] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+            if (board[1][0] == 'O'){
+                if (board[2][0] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+            if (board[1][1] == 'O'){
+                if (board[2][2] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+        }
+
+        if(board[1][1] == 'X'){
+            if (board[1][0] == 'X'){
+                if (board[1][2] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+            if (board[0][1] == 'X'){
+                if (board[2][1] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+            if (board[0][2] == 'X'){
+                if (board[2][0] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+        }
+
+        else if(board[1][1] == 'O'){
+            if (board[1][1] == 'O'){
+                if (board[1][2] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+            if (board[0][1] == 'O'){
+                if (board[2][1] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+            if (board[0][2] == 'O'){
+                if (board[2][0] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+        }
+
+        if(board[2][2] == 'X'){
+            if (board[1][2] == 'X'){
+                if (board[0][2] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+            if (board[2][1] == 'X'){
+                if (board[2][0] == 'X'){
+                    System.out.println('X');
+                    return 'X';
+                }
+            }
+        }
+
+        else if(board[2][2] == 'O'){
+            if (board[1][2] == 'O'){
+                if (board[0][2] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+            if (board[2][1] == 'O'){
+                if (board[2][0] == 'O'){
+                    System.out.println('O');
+                    return 'O';
+                }
+            }
+        }
+        return 'T';
     }
 }
