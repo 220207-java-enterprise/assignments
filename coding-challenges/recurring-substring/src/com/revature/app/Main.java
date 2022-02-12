5d1faab947f54f2e1282ddfddc5ff9016c054265
Aiza W <johndoe@example.com>
2022-02-11 19:39:38 -0700

    recurringSubstring

--- coding-challenges/recurring-substring/src/com/revature/app/Main.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Main.java
@@ -3,5 +3,7 @@ package com.revature.app;
 public class Main {
     public static void main(String[] args) {
         /* Use Main to enter in values into your Solution class */
+        Solution sol = new Solution();
+        System.out.println(sol.recurringSubstring("aabaab"));
     }
 }
--- coding-challenges/recurring-substring/src/com/revature/app/Solution.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Solution.java
@@ -3,6 +3,17 @@ package com.revature.app;
 public class Solution {
     public boolean recurringSubstring(String s) {
         /* Write your code here */
-        return false;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.substring(0,(s.length()/2)));
+
+        //System.out.println(sb.toString()+ " "+s.substring(s.length()/2));
+
+        if(sb.toString().equals(s.substring((s.length()/2)))){
+
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
