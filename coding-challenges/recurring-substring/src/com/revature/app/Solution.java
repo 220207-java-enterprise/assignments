f3413e36fab839d27b78debd2e96718e47df5946
zhenying <a664330348@gmail.com>
2022-02-12 13:41:59 -0500

    completed tic-tac-toe

--- coding-challenges/recurring-substring/src/com/revature/app/Solution.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Solution.java
@@ -3,6 +3,22 @@ package com.revature.app;
 public class Solution {
     public boolean recurringSubstring(String s) {
         /* Write your code here */
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            sb.append(s.charAt(i));
+            String substr = sb.toString();
+
+            while (sb.length() <= s.length()) {
+                if (sb.toString().equals(s)) {
+                    return true;
+                } else {
+                    sb.append(substr);
+                }
+            }
+
+            sb = new StringBuilder(substr);
+        }
         return false;
     }
 }
--- coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Solution.java
+++ coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Solution.java
@@ -2,6 +2,24 @@ package com.revature.tictactoe;
 
 public class Solution {
     public char validateWin(char[][] board) {
-        return ' ';
+        if (board[0][0] == board[0][1] && board[0][1]== board[0][2])
+            return  board[0][0];
+        else if (board[1][0] == board[1][1] && board[1][1]== board[1][2])
+            return  board[1][0];
+        else if (board[2][0] == board[2][1] && board[2][1]== board[2][2])
+            return  board[2][0];
+        else if (board[0][0] == board[1][0] && board[1][0]== board[2][0])
+            return  board[0][0];
+        else if (board[0][1] == board[1][1] && board[1][1]== board[2][1])
+            return  board[0][1];
+        else if (board[0][2] == board[1][2] && board[1][2]== board[2][2])
+            return  board[0][2];
+        else if (board[0][0] == board[1][1] && board[1][1]== board[2][2])
+            return  board[0][0];
+        else if (board[0][2] == board[1][1] && board[1][1]== board[2][0])
+            return  board[0][2];
+        else
+            return ' ';
     }
 }
+
