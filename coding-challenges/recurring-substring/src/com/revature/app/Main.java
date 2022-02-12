008200c8f2a3d6e8ced7a1c111694916ebb5ff45
Aidan Amato <aidanamato@comcast.net>
2022-02-12 14:29:01 -0500

    solved recurring-substring coding challenge

--- coding-challenges/recurring-substring/src/com/revature/app/Main.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Main.java
@@ -3,5 +3,6 @@ package com.revature.app;
 public class Main {
     public static void main(String[] args) {
         /* Use Main to enter in values into your Solution class */
+        System.out.println(new Solution().recurringSubstring("abcabc"));
     }
 }
