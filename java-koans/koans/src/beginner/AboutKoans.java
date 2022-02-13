6a13c7b0eb9894042908e4555b3a16f00e9de163
Amelia DePew <GuayaRainbow@Amelias-MBP-4.lan>
2022-02-13 11:05:47 -1000

    My first koan

--- java-koans/koans/src/beginner/AboutKoans.java
+++ java-koans/koans/src/beginner/AboutKoans.java
@@ -5,18 +5,21 @@ import com.sandwich.koan.Koan;
 import static com.sandwich.util.Assert.fail;
 
 public class AboutKoans {
-
     @Koan
-    public void findAboutKoansFile() {
-        fail("delete this line to advance");
+    public void findAboutKoansFile () {
+
     }
-
     @Koan
-    public void definitionOfKoanCompletion() {
-        boolean koanIsComplete = false;
-        if (!koanIsComplete) {
-            fail("what if koanIsComplete variable was true?");
+    public void definitionOfKoanCompletion () {
+            boolean koanIsComplete = true;
+            if (!koanIsComplete) {
+                fail("what if koanIsComplete variable was true?");
+            }
         }
+
     }
+
+
+
 
 }
