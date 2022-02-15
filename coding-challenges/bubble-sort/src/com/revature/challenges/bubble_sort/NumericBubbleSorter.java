8ea6ef3baa21abe7564b37b92981f40713fab4d9
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-02-15 13:46:17 -0800

    Bubble Sort

--- coding-challenges/bubble-sort/src/com/revature/challenges/bubble_sort/NumericBubbleSorter.java
+++ coding-challenges/bubble-sort/src/com/revature/challenges/bubble_sort/NumericBubbleSorter.java
@@ -13,6 +13,14 @@ public class NumericBubbleSorter implements Sorter<Integer> {
      */
     @Override
     public void sort(Integer[] collection) throws IllegalArgumentException {
+        for (int i = 0; i < collection.length-1; i++){
+            if (collection[i]>collection[i+1]){
+                int tmp = collection[i+1];
+                collection[i+1] = collection[i];
+                collection[i] = tmp;
+            }
+
+        }
         // TODO your implementation
     }
 
