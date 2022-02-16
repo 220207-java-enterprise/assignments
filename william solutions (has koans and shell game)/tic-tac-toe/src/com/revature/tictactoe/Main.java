9d372744d2cb7a656f88baaa6914c37ed557a1d5
wij2012 <wij2012@gmail.com>
2022-02-16 10:53:37 -0500

    housekeeping, moved some stuff around and changed directory names

--- william solutions (has koans and shell game)/tic-tac-toe/src/com/revature/tictactoe/Main.java
+++ william solutions (has koans and shell game)/tic-tac-toe/src/com/revature/tictactoe/Main.java
@@ -3,5 +3,7 @@ package com.revature.tictactoe;
 public class Main {
     public static void main(String[] args) {
         System.out.println("a;lsdkf");
+        char[][] board = new char[3][3];
+
     }
 }
