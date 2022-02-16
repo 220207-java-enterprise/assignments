505cafd98bdca6b3f2e94e2b9d97e3c9f14bace9
Wezley Singleton <wezley.singleton@gmail.com>
2022-02-15 21:39:41 -0500

    tic tac toe solution

--- coding-challenges/wezley_solutions/tic-tac-toe/src/com/revature/tictactoe/Solution.java
+++ coding-challenges/wezley_solutions/tic-tac-toe/src/com/revature/tictactoe/Solution.java
@@ -2,6 +2,23 @@ package com.revature.tictactoe;
 
 public class Solution {
     public char validateWin(char[][] board) {
-        return ' ';
+
+        // Check horizontals
+        if (board[0][0] == board[0][1] && board[0][1] == board[0][2]) return board[0][0];
+        if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) return board[1][0];
+        if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) return board[2][0];
+
+        // Check verticals
+        if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) return board[0][0];
+        if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) return board[0][1];
+        if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) return board[0][2];
+
+        // Check diagonals
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0];
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2];
+
+        // If no returns by this point, it is a tie
+        return 'T';
+
     }
 }
