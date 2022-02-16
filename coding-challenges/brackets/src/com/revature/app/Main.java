3829ec613e2edeb03eb05844e1791fc6ac73a98f
Aiza W <johndoe@example.com>
2022-02-15 17:34:28 -0700

    new folder with assignments

--- coding-challenges/brackets/src/com/revature/app/Main.java
+++ coding-challenges/brackets/src/com/revature/app/Main.java
@@ -3,5 +3,7 @@ package com.revature.app;
 public class Main {
     public static void main(String[] args) {
         /* Use this to enter in values for your Solution class */
+        Solution sol = new Solution();
+        System.out.println(sol.brackets("abc.abc"));
     }
 }
--- coding-challenges/brackets/src/com/revature/app/Solution.java
+++ coding-challenges/brackets/src/com/revature/app/Solution.java
@@ -3,6 +3,9 @@ package com.revature.app;
 public class Solution {
     public String brackets(String s) {
         /* Write your code here */
-        return "";
+        if(s.contains(".")==true){
+            s=s.replace(".","[.]");
+        }
+        return s;
     }
 }
