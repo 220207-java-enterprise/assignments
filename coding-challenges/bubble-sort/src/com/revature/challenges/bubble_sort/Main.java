69ac8acdfc4d3be0230dfe5a6e549021df23cc91
usertest0447 <paulsmith414@yahoo.com>
2022-02-15 18:46:01 -0600

    added printArray method to the interface

---
+++ coding-challenges/bubble-sort/src/com/revature/challenges/bubble_sort/Main.java
@@ -0,0 +1,2 @@
+package com.revature.challenges.bubble_sort;public class Main {
+}
--- coding-challenges/bubble-sort/src/com/revature/challenges/bubble_sort/Sorter.java
+++ coding-challenges/bubble-sort/src/com/revature/challenges/bubble_sort/Sorter.java
@@ -1,5 +1,8 @@
 package com.revature.challenges.bubble_sort;
 
 public interface Sorter<T> {
+
+    void printArray(T[] collection);
     void sort(T[] collection);
+
 }
