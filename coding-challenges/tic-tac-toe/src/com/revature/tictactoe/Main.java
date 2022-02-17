246e4feea4e76e69785c47c67a7d3573780ca280
wij2012 <wij2012@gmail.com>
2022-02-17 12:51:58 -0500

    worked on tictactoe

--- coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Main.java
+++ coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Main.java
@@ -2,5 +2,6 @@ package com.revature.tictactoe;
 
 public class Main {
     public static void main(String[] args) {
+        char[][] board = new char[3][3];
     }
 }
--- coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Solution.java
+++ coding-challenges/tic-tac-toe/src/com/revature/tictactoe/Solution.java
@@ -1,7 +1,51 @@
 package com.revature.tictactoe;
 
 public class Solution {
+    public char checkRows(char[] boardRow) {
+        char haveAWinner = 'n';
+        for(int i=0; i<boardRow.length-1; i++){
+            if(boardRow[i] == boardRow[i+1])
+                haveAWinner = boardRow[i];
+            else {
+                haveAWinner = 'n';
+                return haveAWinner;
+            }
+        }
+        return haveAWinner;
+    }
+
+    public char checkCols(char[][] board) {
+        char haveAWinner = 'n';
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+
+            }
+        }
+        return haveAWinner;
+    }
+
+    public char checkDiagonals(char[][] board) {
+        return 'a';
+    }
+
     public char validateWin(char[][] board) {
-        return ' ';
+        char win = 'n';
+
+        //check rows for a win
+        for(int i = 0; i<board.length;i++) {
+            win = checkRows(board[i]);
+            if (win == 'O' || win == 'X')
+                return win;
+        }
+
+        //check columns for winner
+        win = checkCols(board);
+        if(win == 'O' || win == 'X')
+            return win;
+
+        //check diagonals for winner
+        win = checkDiagonals(board);
+
+        return win;
     }
 }
