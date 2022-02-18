6a5df4801cd48324f284f90e921f04a854a046b7
one5los <carlos.iniguez17@gmail.com>
2022-02-18 07:35:28 -0800

    renamed coding-challenges folder to resolve merge

---
+++ carlos-coding-challenges/brackets/src/com/revature/app/Main.java
@@ -0,0 +1,6 @@
+package com.revature.app;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ carlos-coding-challenges/brackets/src/com/revature/app/Solution.java
@@ -0,0 +1,16 @@
+package com.revature.app;
+
+import java.util.*;
+
+public class Solution {
+    public String brackets() {
+        System.out.println("Enter a string with as many periods as you'd like: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+
+        userInput = userInput.replaceAll("\\.", "[.]");
+        return userInput;
+    }
+}
+
