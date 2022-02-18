45c5ac6fba18dc57e3883b3a7d55c04b2fdf1623
pksg21 <paulsmith414@yahoo.com>
2022-02-17 20:47:48 -0800

    adding solution and testing new windows setup

---
+++ coding-challenges/length-of-string/LengthOfString.java
@@ -0,0 +1,6 @@
+class LengthOfString{
+    public static void main(String[] ags){
+        String name = "This is a String";
+	System.out.println(name.charAt(0) + "" + (name.length() - 2) + "" + name.charAt(name.length() - 1));
+    }
+}
