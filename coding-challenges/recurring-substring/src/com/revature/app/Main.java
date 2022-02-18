bb29adb4726a845992526559014f840ab4d9a6c2
Cameron Lintz <cameron034@revature.net>
2022-02-18 12:41:16 -0500

    git push test

--- coding-challenges/recurring-substring/src/com/revature/app/Main.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Main.java
@@ -1,5 +1,5 @@
 package com.revature.app;
-
+//testing git bash
 public class Main {
     public static void main(String[] args) {
         /* Use Main to enter in values into your Solution class */
--- coding-challenges/recurring-substring/src/com/revature/app/Solution.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Solution.java
@@ -2,6 +2,7 @@ package com.revature.app;
 
 public class Solution {
     public boolean recurringSubstring(String s) {
+
         /* Write your code here */
         return false;
     }
