b67042027ee81bf0215661801705b973f1eaebb9
pksg21 <paulsmith414@yahoo.com>
2022-02-19 14:49:01 -0800

    readding solution to recurringsubstring

--- coding-challenges/recurring-substring/src/com/revature/app/Main.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Main.java
@@ -5,10 +5,16 @@ public class Main {
         /* Use Main to enter in values into your Solution class */
         String a = "abababa";
         String b = "abcdabcdabcdabcdabcd";
+        String c = "";
+        String d = "                ";
+        String status = " contains repeating characters: ";
         Solution s = new Solution();
 
-        System.out.println(s.recurringSubstring(a));
-        System.out.println(s.recurringSubstring(b));
+        System.out.println("Test cases:");
+        System.out.println("String " + a + status + s.recurringSubstring(a));
+        System.out.println("String " + b + status + s.recurringSubstring(b));
+        System.out.println("String " + c + status + s.recurringSubstring(c));
+        System.out.println("String " + d + status + s.recurringSubstring(d));
 
     }
 }
--- coding-challenges/recurring-substring/src/com/revature/app/Solution.java
+++ coding-challenges/recurring-substring/src/com/revature/app/Solution.java
@@ -2,12 +2,30 @@ package com.revature.app;
 
 public class Solution {
     public boolean recurringSubstring(String s) {
+
         /* Write your code here */
-        if(s.isBlank())
+
+        // covers empty string and whitespaces string cases
+        if (s.isBlank())
             return false;
-        String operation = s.substring(1) + s.substring(0, s.length()-1);
+        /*
+         when a string is added to itself and the first and the last characters are removed
+         from that string.
+         If that new string contains the original string, the original string has a repeating pattern
+         ex: abcabc
+         addition abcabc + abcabc
+         altered string bc abc abc ab
+         the repeating substring is in the middle
+         String operation = s + s;
+         operation = operation.substring(1, operation.length() -1);
+        */
+       String operation = s.substring(1) + s.substring(0, s.length()-1);
+
+        // covers recurring string cases
         if (operation.contains(s))
             return true;
+
+        // covers all the other cases
         return false;
     }
 }
