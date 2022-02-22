47ee54fb4fcc238023dbe979ebee301c83973a6c
abhilekhx <lekh7adhikari@gmail.com>
2022-02-21 23:24:23 -0500

    finished recurring-word

--- coding-challenges/recurring-word/src/main/java/com/revature/recurringWord/Main.java
+++ coding-challenges/recurring-word/src/main/java/com/revature/recurringWord/Main.java
@@ -2,5 +2,9 @@ package com.revature.recurringWord;
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.recurringWord("aabsdaaaadfasaaa3123212"));
+
     }
 }
--- coding-challenges/recurring-word/src/main/java/com/revature/recurringWord/Solution.java
+++ coding-challenges/recurring-word/src/main/java/com/revature/recurringWord/Solution.java
@@ -1,7 +1,37 @@
 package com.revature.recurringWord;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public char recurringWord(String str) {
-        return ' ';
+        //int count = frequency.containsKey(str.charAt(i))? frequency.get(str.charAt(i)) : 0;
+
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        int max=0;
+        char result=' ';
+
+        for(int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+            if (frequency.containsKey(c)){
+                int count = frequency.get(str.charAt(i));
+                frequency.put(c, ++count);
+            } else{
+                frequency.put(c, 1);
+            }
+            // gets max value inside frequency
+            max = Collections.max(frequency.values());
+        }
+
+        // search for key with max counter value
+        for (Map.Entry<Character, Integer> entry: frequency.entrySet()){
+            if(entry.getValue()==max){
+                result = entry.getKey();
+            }
+        }
+
+        return result;
     }
 }
+
