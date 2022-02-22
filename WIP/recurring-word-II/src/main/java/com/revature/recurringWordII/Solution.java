2f7bbf62530a4791d1ae5bfbe82a9a285f86a7c4
Adam Lyn <adam273@revature.net>
2022-02-21 23:14:09 -0500

    Recurring word2 complete

--- WIP/recurring-word-II/src/main/java/com/revature/recurringWordII/Solution.java
+++ WIP/recurring-word-II/src/main/java/com/revature/recurringWordII/Solution.java
@@ -1,7 +1,40 @@
 package com.revature.recurringWordII;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Solution {
     public String recurringWordII(String str) {
-        return "";
+        String[] split = str.split(" ");
+        ArrayList<String> myList = new ArrayList<>();
+        int counter = 1, highestCount = 0;
+        String current;
+        String mostString = "";
+
+        for(String s: split)
+            myList.add(s);
+        //System.out.println(myList + " " + myList.size());
+        Collections.sort(myList);
+        //System.out.println(myList + " " + myList.size());
+
+        current = myList.get(0);
+        for(int j = 1; j < myList.size();j++)
+        {
+            if (myList.get(j).equals(current)){
+                //System.out.println(current + " " + myList.get(j));
+                counter++;
+            }
+            else {
+                //System.out.println(current + " " + myList.get(j));
+                if (counter > highestCount) {
+                    highestCount = counter;
+                    mostString = myList.get(j-1);
+                    //System.out.println(counter+ " " + mostString);
+                }
+                counter = 1;
+                current = myList.get(j-1);
+            }
+        }
+        return mostString;
     }
 }
