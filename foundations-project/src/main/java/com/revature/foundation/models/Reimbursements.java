62505d79281577d8762cbdf832e3664f075f38de
Gmander <aidanfire360@gmail.com>
2022-03-01 16:56:44 -0500

    init?

--- foundations-project/src/main/java/com/revature/foundation/models/Reimbursements.java
+++
@@ -1,4 +0,0 @@
-package com.revature.foundation.models;
-
-public class Reimbursements {
-}
--- foundations-project/src/main/java/com/revature/foundation/util/exceptions/ResourcePersistenceException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.foundation.util.exceptions;
-
-public class ResourcePersistenceException extends RuntimeException {
-
-    public ResourcePersistenceException(String msg) {
-        super(msg);
-    }
-
-}
