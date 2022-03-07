eccfc4b8720e9012a95cbf2a9566eb9634ce58fd
= <=>
2022-03-07 11:58:18 -0800

    tidying up

--- coding-challenges/finding-cycles/src/main/java/com/revature/challenges/finding_cycles/SinglyLinkedList.java
+++ coding-challenges/finding-cycles/src/main/java/com/revature/challenges/finding_cycles/SinglyLinkedList.java
@@ -1,5 +1,8 @@
 package com.revature.challenges.finding_cycles;
 
+import java.util.LinkedList;
+import java.util.ArrayList;
+
 public class SinglyLinkedList<T> {
 
     protected Node<T> head;
@@ -15,9 +18,47 @@ public class SinglyLinkedList<T> {
      *
      * @return true if a cycle is present within this list, otherwise false
      */
-    public boolean containsCycle() {
-        // TODO your implementation goes here
-        return false;
+    public boolean containsCycle(SinglyLinkedList<T> ll) {
+        // Here goes!
+        ArrayList<Node> al = new ArrayList<>();
+        // Start at head node
+        Node temp = new Node("Snoopy");
+        temp = ll.head;
+        if (temp.nextNode == null) {
+            return false;
+        }
+        if (temp.nextNode == head) {
+            return true;
+        }
+
+        Node next = temp.nextNode;
+
+        al.add(temp);
+        //al.add(next);
+
+        do {
+
+            for (int i = 0; i < al.size(); i++) {
+
+                if (temp.nextNode == al.get(i)) {
+                    return true;
+                }
+            }
+            // At this point we know that the node in question hasn't occurred in the list before.
+
+            // go to the next node
+            al.add(next);
+            temp = next;
+            next = temp.nextNode;
+        } while (temp.nextNode != null);
+
+        // If its reference data shows up in the array list, return true.
+
+        // else place its reference data into the array list
+
+        // Last node?  No?  Go back up and go to the next node to check it
+
+        return false; // There are no cycles in this linked list.
     }
 
     public static class Node<T> {
--- coding-challenges/finding-cycles/src/main/java/com/revature/challenges/finding_cycles/TestForLoop.java
+++ coding-challenges/finding-cycles/src/main/java/com/revature/challenges/finding_cycles/TestForLoop.java
@@ -1,2 +1,16 @@
-package com.revature.challenges.finding_cycles;public class TestForLoop {
+package com.revature.challenges.finding_cycles;
+
+public class TestForLoop {
+    public static void main(String[] args) {
+        System.out.println("Setting up a node called nodule.");
+        SinglyLinkedList.Node<String> nodule = new SinglyLinkedList.Node<>("Snoopy");
+        System.out.println("Setting up a singly linked list object.");
+        SinglyLinkedList sll = new SinglyLinkedList(nodule);
+
+        System.out.println("Testing to see if sll contains a loop.");
+        if (sll.containsCycle(sll))
+            System.out.println("Yes, the linked list contains a loop.");
+        else
+            System.out.println("no, the linked list does not contain a loop.");
+    }
 }
